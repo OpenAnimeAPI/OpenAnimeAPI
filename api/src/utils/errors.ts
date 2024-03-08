7dab4ed311bfd1cf42d874a11d48d9fc1c608cd6
Stefan Fischer <stefischer@mail.com>
2024-03-08 03:16:57 -0500

    Add pagination and errors utils

---
+++ api/src/utils/errors.ts
@@ -0,0 +1,60 @@
+import type { Response } from '@@types/express.js';
+import type { 
+    SendResponseOptions, 
+    ErrorLogOptions, 
+    HandleTupleOptions, 
+    SendEntitiesResponseParams 
+} from '@@types/errors.js';
+
+import * as logs from './logs.js';
+import * as colors from './colors.js';
+
+export function sendResponse({ res, next, err, status=500, message }: SendResponseOptions): void {
+    const logOptions: ErrorLogOptions = {
+        color: colors.error
+    };
+
+    if(err) {
+        logOptions.err = err;
+    }
+
+    if(message) {
+        logOptions.message = message;
+    }
+
+    if(err && message) {
+        logs.error(logOptions);
+    }
+
+    if(next && err) {
+        next(err);
+    }
+    else {
+        res.status(status).json({ error: true, message });
+    }
+};
+
+export function handleTuple<T>({res, err, errMsg}: HandleTupleOptions<T>): Error | undefined {    
+    if(err) {
+        return err;
+    }
+    else if(!res) {
+        return new Error(errMsg);
+    }
+ 
+    return undefined;
+};
+
+export function sendEntitiesResponse<T>({ res, err, message, entityReturn, missingEntityReturnMessage }: SendEntitiesResponseParams<T>) {
+    if(err) {
+        return sendResponse({ res, err, status: 500, message: message ?? err.message });
+    }
+
+    if(!entityReturn) {
+        return sendResponse({ res, status: 404, message: missingEntityReturnMessage });
+    }
+};
+
+export function sendInvalidBody(res: Response) {
+    return sendResponse({ res, status: 400, message: "Invalid Body" });
+};
--- api/src/utils/index.ts
+++ api/src/utils/index.ts
@@ -1,5 +1,7 @@
 export * as colors from './colors.js';
 export * as common from './common.js';
 export * as entities from './entities.js';
+export * as errors from './errors.js';
 export * as logs from './logs.js';
+export * as pagination from './pagination.js';
 export * as promises from './promises.js';
