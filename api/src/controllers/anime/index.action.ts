1c4cf1fb65d19545d4b2dbcdda5bbaaf39512c35
JPetsis <snowblind777@gmail.com>
2024-03-28 00:06:41 +0100

    Add anime routes

---
+++ api/src/controllers/anime/index.action.ts
@@ -0,0 +1,6 @@
+import { Request, Response, NextFunction } from 'express';
+
+async function index(req: Request, res: Response, next: NextFunction) {
+};
+
+export default index;
---
+++ api/src/routes/index.ts
@@ -0,0 +1,1 @@
+export { default as animeRoutes} from '@@root/routes/anime/animeRoutes';
