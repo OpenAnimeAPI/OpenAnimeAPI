f8bf731238bd9cb665fdb26b6ec0bee6bfcb1b27
Stefan Fischer <stefischer@mail.com>
2024-03-27 13:06:20 -0400

    Remove custom many to many tables

--- api/src/entities/index.ts
+++ api/src/entities/index.ts
@@ -1,7 +1,5 @@
 export { default as Anime } from './Anime.js';
-export { default as AnimeCharacter } from './AnimeCharacter.js';
-export { default as AnimeCharacterVoiceActor } from './AnimeCharacterVoiceActor.js';
-export { default as AnimeStudio } from './AnimeStudio.js';
 export { default as Character } from './Character.js';
 export { default as Studio } from './Studio.js';
 export { default as User } from './User.js';
+export { default as VoiceActor } from './VoiceActor.js';
