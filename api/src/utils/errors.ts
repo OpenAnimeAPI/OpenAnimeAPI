dbec965f14c013f628f948074a4b45aae744f8cd
Stefan Fischer <stefischer@mail.com>
2024-04-06 12:45:53 -0400

    Add zodIssues to invalidBody errors utils method

--- api/src/utils/errors.ts
+++ api/src/utils/errors.ts
@@ -1,3 +1,4 @@
+import type { ZodError } from 'zod';
 import type { Response } from '@@types/express.js';
 import type { 
     SendResponseOptions, 
@@ -9,7 +10,7 @@ import type {
 import * as logs from './logs.js';
 import * as colors from './colors.js';
 
-export function sendResponse({ res, next, err, status=500, message }: SendResponseOptions): void {
+export function sendResponse({ res, next, err, status=500, message, issues }: SendResponseOptions): void {
     const logOptions: ErrorLogOptions = {
         color: colors.error
     };
@@ -30,7 +31,11 @@ export function sendResponse({ res, next, err, status=500, message }: SendRespon
         next(err);
     }
     else {
-        res.status(status).json({ error: true, message });
+        res.status(status).json({ 
+            error: true, 
+            message, 
+            ...(issues && { issues }) 
+        });
     }
 };
 
@@ -45,16 +50,21 @@ export function handleTuple<T>({res, err, errMsg}: HandleTupleOptions<T>): Error
     return undefined;
 };
 
-export function sendEntitiesResponse<T>({ res, err, message, entityReturn, missingEntityReturnMessage }: SendEntitiesResponseParams<T>) {
+export function sendEntitiesResponse<T>({ res, err, message, entity, missingEntityMessage }: SendEntitiesResponseParams<T>) {
     if(err) {
         return sendResponse({ res, err, status: 500, message: message ?? err.message });
     }
 
-    if(!entityReturn) {
-        return sendResponse({ res, status: 404, message: missingEntityReturnMessage });
+    if(!entity) {
+        return sendResponse({ res, status: 404, message: missingEntityMessage });
     }
 };
 
-export function sendInvalidBody(res: Response) {
-    return sendResponse({ res, status: 400, message: "Invalid Body" });
+export function sendInvalidBody<T>(res: Response, zodError: ZodError<T>, message?: string) {
+    return sendResponse({ 
+        res, 
+        status: 400, 
+        message: message ?? "Invalid Body", 
+        issues: zodError.errors 
+    });
 };
