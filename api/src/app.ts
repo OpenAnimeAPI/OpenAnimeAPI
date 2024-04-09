263658f61c5eab252cf2718cd1ce42ff2eeafbcd
Stefan Fischer <stefischer@mail.com>
2024-04-08 20:18:04 -0400

    Add voice_actor url path to app

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,6 +2,8 @@ import express, { Application } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 
+import { voiceActorRoutes } from './routes/index.js';
+
 function initializeApp(): Application {
     const app = express();
 
@@ -11,6 +13,8 @@ function initializeApp(): Application {
     app.use(cors());
     app.set("trust proxy", true);
     app.set("trust proxy", "loopback");
+
+    app.use("/voice_actors", voiceActorRoutes);
 
     return app;
 };
