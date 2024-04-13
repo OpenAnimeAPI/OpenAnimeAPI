db4e20ea56f1b4c6bc3d388ef4bad230f9cc81e7
Stefan Fischer <stefischer@mail.com>
2024-04-13 04:03:47 -0400

    Add more voice actor test payloads

--- api/src/__tests__/support/payloads/voiceActor.payloads.ts
+++ api/src/__tests__/support/payloads/voiceActor.payloads.ts
@@ -3,3 +3,17 @@ export const VALID_CREATE = {
     last_name: "Everest",
     country: "United States"
 };
+
+export const INVALID_CREATE = {
+    first_name: 81729369
+};
+
+export const VALID_UPDATE = {
+    first_name: "Monster",
+    last_name: "Energy",
+    country: "Finland"
+};
+
+export const INVALID_UPDATE = {
+    first_name: 9918272
+};
